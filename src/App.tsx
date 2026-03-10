3854bf486589fa75559726aa56b71719c8231088
Pavel <samasyt@mail.ru>
2026-03-10 15:17:43 +0300

    feat: added Lazy routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Counter from "./components/Counter";
 import "./index.scss";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import { Link } from "react-router-dom";
+import { MainPageLazy } from "./pages/MainPage/MainPageLazy";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPageLazy";
 
 const App = () => {
   return (
     <div className="app">
-      asdasdasd
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageLazy />} />
+          <Route path="/about" element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
