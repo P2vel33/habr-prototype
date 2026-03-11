0dc5ce35ae7e9376cf446f1da5223cc9ff18e204
Pavel <samasyt@mail.ru>
2026-03-11 17:16:03 +0300

    fix: useTheme( ts error )

--- src/theme/useTheme.ts
+++ src/theme/useTheme.ts
@@ -9,7 +9,7 @@ export function useTheme(): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
   const toggleTheme = () => {
     const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
-    setTheme(newTheme);
+    setTheme!(newTheme);
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 
