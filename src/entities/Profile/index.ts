c6bccf1a3f829dd506a78bf4e4eff943931b5cf8
Pavel Fedotov <samasyt@mail.ru>
2026-06-30 23:52:52 +0300

    upd: added getting profile data

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,4 +1,5 @@
 export { Profile } from "./model/types/profile";
 export { ProfileSchema } from "./model/types/profile";
+export { fetchProfileData } from "./model/services/fetchProfileData/fetchProfileData";
 
 export { profileActions, profileReducer } from "./model/slice/profileSlice";
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,10 +1,12 @@
 import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
 import { classNames } from "@/shared/lib/classNames/classNames";
 import {
     DynamicModalLoader,
     ReducersList,
 } from "@/shared/lib/components/DynamicModalLoader/DynamicModalLoader";
-import { profileReducer } from "@/entities/Profile";
+import { fetchProfileData, profileReducer } from "@/entities/Profile";
+import { useAppDispatch } from "@/shared/lib/hooks/useAppDispatch/useAppDispatch";
 
 export interface ProfilePageProps {
     className?: string;
@@ -16,6 +18,11 @@ const redusers: ReducersList = {
 
 const ProfilePage = ({ className = "" }: ProfilePageProps) => {
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
     return (
         <DynamicModalLoader reducers={redusers} removeAfterUnmount>
             <div className={classNames("", {}, [className])}>
